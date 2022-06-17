eba0d8cb6c280ac338ae8856164c32b9a5a10e49
Jonathan Pena <98781952+JonathanPena17@users.noreply.github.com>
2022-06-17 01:22:36 -0700

    Add import packages

--- portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
@@ -1,3 +1,28 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/translate")
 public class TranslationServlet extends HttpServlet {
 
@@ -10,7 +35,7 @@ public class TranslationServlet extends HttpServlet {
     // Do the translation.
     Translate translate = TranslateOptions.getDefaultInstance().getService();
     Translation translation =
-        translate.translate(originalText, Translate.TranslateOption.targetLanguage(ru));
+        translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCode));
     String translatedText = translation.getTranslatedText();
 
     // Output the translation.
